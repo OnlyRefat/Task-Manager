d1644a1d2b8d11e5105e88e7f2824665f5cdd5b4
mhasan3 <mahmudul69@hotmail.com>
2015-10-12 00:42:17 +0600

    fetched developer list to admin template

--- js/controller/AdminController.js
+++ js/controller/AdminController.js
@@ -14,18 +14,14 @@ $scope.getdata=function()
 
 	query.find({
 	  success: function(results) {
-	    // alert("Successfully retrieved " + results.length + " tickets.");
-	    // console.log(JSON.stringify(results));
-
-	    // Do something with the returned Parse.Object values
+	    
 	    for (var i = 0; i < results.length; i++) {
-	    	console.log(results[i]);
+	    	// console.log(results[i]);
 	      var object = results[i];
 	      $scope.tickets.push({"title":results[i].get('title'),"id":results[i].id});
-	      // alert(object.id + ' - ' + object.get('title'));
 	    }
 
-	    console.log($scope.tickets);
+	    // console.log($scope.tickets);
 	    $scope.$apply();
 	  },
 	  error: function(error) {
@@ -34,18 +30,51 @@ $scope.getdata=function()
 	});
 
 
-	var dev = 'developer';
+	// var dev = 'developer';
 
-	query = new Parse.Query(Parse.Role);
-    query.equalTo ("name",dev);
+	// query = new Parse.Query(Parse.Role);
+ //    query.equalTo ("name",dev);
 
-	query.find({
-		success: function(results){
-			console.log(JSON.parse(JSON.stringify(results)));
-			console.log(Parse.Role.get('users'));
+	// query.find({
+	// 	success: function(results){
+	// 		// console.log(JSON.parse(JSON.stringify(results)));
+	// 		// console.log(Parse.Role.get('users'));
 			
-		}
-	})
+	// 	}
+	// })
+
+		$scope.roleUser = [];
+
+		query = (new Parse.Query(Parse.Role));
+		query.equalTo("name", "developer");
+		query.first({ success: function(role) {
+		  role.relation('users').query().find({success: function(users) {
+		  	// console.log(users);
+		  	
+		  	
+		  	// $scope.roleUser.push({"id": users.id});
+		  	for (var i = 0; i < users.length; i++) {
+	    	
+		  	query = (new Parse.Query(Parse.User));
+			query.equalTo("objectId", users[i].id);
+			query.find({
+				success: function(res){
+					var userNamesInDev = JSON.parse((JSON.stringify(res)));
+					$scope.roleUser.push(userNamesInDev[0].username);
+					// $scope.$apply();
+				}
+			})
+		  	// console.log(users[i].id);
+	    }
+		  	
+		  },
+		  	
+
+		  	error: function(error) {
+	    alert("Error: " + error.code + " " + error.message);
+	  }
+		})
+	}})
 
 }
 
